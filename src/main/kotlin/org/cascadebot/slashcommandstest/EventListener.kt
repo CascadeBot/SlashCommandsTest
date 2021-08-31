b555a68df256aaaf98f23a6ef3b008e65de199f1
weeryan17 <theweeryan17@gmail.com>
2021-08-31 09:11:27 -0500

    Make the event listener work again

--- src/main/kotlin/org/cascadebot/slashcommandstest/EventListener.kt
+++ src/main/kotlin/org/cascadebot/slashcommandstest/EventListener.kt
@@ -13,24 +13,10 @@ class EventListener : ListenerAdapter() {
     override fun onSlashCommand(event: SlashCommandEvent) {
         val args = CommandArgs(event.options.groupBy { it.name })
 
-        val path = event.commandPath
-        val command = getCommand(path) ?: return // TODO reply with some sort of message saying the command doesn't exist
+        val command = SlashCommandsTest.commandManager.getCommand(event.commandIdLong, event.commandPath) ?: return // TODO reply with some sort of message saying the command doesn't exist
 
         event.deferReply()
         command.onCommand(CommandContext(event), args, Object())
-    }
-
-    fun getCommand(path: String): ExecutableCommand? {
-        val parts = path.split("/")
-        var command: ExecutableCommand? = null;
-        if (parts.size == 1) {
-            command = SlashCommandsTest.commandManager.commands.first { it.command == parts[0] }
-        } else if (parts.size == 2) {
-            command = SlashCommandsTest.commandManager.commands.first { it is SubCommand && it.command == parts[1] && it.parent.command == parts[0] }
-        } else if (parts.size == 3) {
-            command = SlashCommandsTest.commandManager.commands.first { it is SubCommand && it.command == parts[2] && it.group != null && it.group.groupName == parts[1] && it.parent.command == parts[0] }
-        }
-        return command
     }
 
 }
